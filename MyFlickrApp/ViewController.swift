af586932166555acb7610e618db6b82a0eb2ddbb
Manuel Mouta <moutamanuel26@gmail.com>
2017-03-03 15:45:20 +0000

    First Commit

---
+++ MyFlickrApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFlickrApp
+//
+//  Created by Manuel Mouta on 03/03/17.
+//  Copyright © 2017 Manuel Mouta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
